3e942b5f3b3a0a07196a3a84aeafd7ab0fc5144a
cya107 <colin_yuen@sfu.ca>
2020-10-12 13:40:07 -0700

    Changed Title Screen to be Landscape

--- app/src/main/java/ca/sfu/cmpt295a3/MainActivity.java
+++ app/src/main/java/ca/sfu/cmpt295a3/MainActivity.java
@@ -4,11 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+/**
+ * Initial Driver
+ * Displays Title Screen w/ Animation
+ * Will swap to Main Menu on click or after all animations + 4 seconds
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.title_screen);
     }
 }
