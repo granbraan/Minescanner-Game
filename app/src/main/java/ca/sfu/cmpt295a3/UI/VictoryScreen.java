b4ae3d85932653f87f06a2c9dc01a668c9aac399
cya107 <colin_yuen@sfu.ca>
2020-10-18 16:41:05 -0700

    Added Victory Screen
    Displays the number of scans used as well as congratulate the user on finishing the game

--- app/src/main/java/ca/sfu/cmpt295a3/UI/VictoryScreen.java
+++ app/src/main/java/ca/sfu/cmpt295a3/UI/VictoryScreen.java
@@ -8,11 +8,15 @@ import android.content.Context;
 import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.List;
 
 import ca.sfu.cmpt295a3.R;
+import ca.sfu.cmpt295a3.model.GameLogic;
 
 public class VictoryScreen extends AppCompatActivity {
     private MediaPlayer myPlayer;
@@ -24,6 +28,31 @@ public class VictoryScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.victory_screen);
+
+        setUpText();
+        setUpButton();
+    }
+
+    private void setUpText(){
+        TextView myText = findViewById(R.id.victorySeekCount);
+        String text = "Finished in ";
+        text += GameLogic.getScansUsed();
+        text += " Scans!";
+
+        myText.setText(text);
+    }
+
+    private void setUpButton(){
+        Button myButton = findViewById(R.id.victoryButton);
+        myButton.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                Intent i = MainMenu.makeLaunchIntent(VictoryScreen.this);
+                startActivityForResult(i, 1);
+                overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+            }
+        });
     }
 
     @Override
